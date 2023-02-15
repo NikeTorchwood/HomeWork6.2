af504b7a865661caa9210a7b3da0908417feb57b
NikeTorchwood <105120677+NikeTorchwood@users.noreply.github.com>
2023-02-16 02:43:48 +0700

    its ready

---
+++ HomeWork6.2/Planet.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6._2
+{
+    internal class Planet
+    {
+        public string Name{get;private set;}
+        public int IdFromSun{get; private set;}
+        public int EquatorLength{get; private set;}
+        private Planet _prevPlanet;
+
+        public Planet(string name, int idFromSun, int equatorLength, Planet planet)
+        {
+            Name= name;
+            IdFromSun= idFromSun;
+            EquatorLength= equatorLength;
+            _prevPlanet = planet;
+        }
+    }
+}
